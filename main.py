c6e1dba45808886f62e13e730ad74045e56c98c0
Andrei Bardier <andrey.bardier@gmail.com>
2023-02-16 23:44:55 +0200

    beginning of work

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import random
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Size of arrays
+small_n = 100
+medium_n = 10000
+large_n = 10
+
+# Partially sorted %
+part_n = 0.5
+
+# Generate small, medium, and large arrays
+small_array = [random.randint(-10000, 10000) for _ in range(small_n)]
+medium_array = [random.randint(-10000, 10000) for _ in range(medium_n)]
+large_array = [random.randint(-10000, 10000) for _ in range(large_n)]
+
+# Generate arrays that are already sorted, partially sorted, or completely unsorted
+sorted_array = sorted(large_array)
+partially_sorted_array = sorted(large_array[:round(large_n * part_n)]) + large_array[round(large_n * part_n):]
+unsorted_array = large_array.copy()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print("Small array:", small_array)
+print("Medium array:", medium_array)
+print("Large array:", large_array)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Sorted array:", sorted_array)
+print("Partially sorted array:", partially_sorted_array)
+print("Unsorted array:", unsorted_array)
